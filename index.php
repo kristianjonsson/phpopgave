50a0282bdb1f5ec93d0b3f7a91d7e222d1f93093
KristianEllgaardJonsson <kristian.jonsson2@gmail.com>
2017-10-03 12:12:04 +0200

    index

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>phpopgave</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+    </head>
+    <body>
+        <?php
+       echo "hej verden";
+        ?>
+<div class="col-md-4">
+   <?php echo "Her udskriver php en tekst"; ?>
+</div>
+<div class="col-md-4">
+   <?php echo "Og her udskrives en anden tekst"; ?> 
+</div>
+<?php
+echo "<div class=\"col-md-6\">";
+echo "<span class=\"text-success\">Det her virker bare!!</span>";
+echo "</div>";
+?> 
+        
+        
+        
+        <h1><?php echo" Hvad kan PHP?" ?></h1>
+<h2><?php echo "Her er en liste over nogle af de ting man kan med PHP:" ?></h2>
+<ul>
+  <li><?php echo"PHP kan generere dynamisk indhold på din webside"?></li>
+  <li><?php echo"PHP kan oprette, åbne, læse, skrive, slette og lukke filer på serveren"?></li>
+  <li><?php echo"PHP kan modtage data fra en formular"?></li>
+  <li><?php echo"PHP kan læse, tilføje, redigere og slette data i din database"?></li>
+  <li><?php echo"PHP kan bruges til brugerkontrolleret adgang"?></li>
+</ul>
+
+
+
+
+    </body>
+    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+</html>
